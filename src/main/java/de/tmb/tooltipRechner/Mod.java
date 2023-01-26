03dfa3bf6bff3fda7c7ff6ac0044db5c4010bdf5
TMB_Randy <100315397+tmbRandy@users.noreply.github.com>
2023-01-26 23:19:15 +0100

    Add files via upload

--- src/main/java/de/tmb/tooltipRechner/Mod.java
+++ src/main/java/de/tmb/tooltipRechner/Mod.java
@@ -1,34 +1,60 @@
 package de.tmb.tooltipRechner;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@net.minecraftforge.fml.common.Mod(modid = Mod.MODID, version = Mod.VERSION)
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@net.minecraftforge.fml.common.Mod("tooltiprechner")
 public class Mod
 {
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) { MinecraftForge.EVENT_BUS.register(Mod.this); }
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
 
-    public static final String MODID = "tooltipRechner";
-    public static final String VERSION = "1.0";
+    public Mod() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
     @SubscribeEvent
     public void onRenderTooltip(ItemTooltipEvent event) {
-        ItemStack stack = event.itemStack;
+        ItemStack stack = event.getItemStack();
 
-        if(stack.hasTagCompound()) {
 
-            if (stack.getTagCompound().hasKey("currentAmount") || stack.getTagCompound().hasKey("stackSize")) {
+        if(stack.hasTag()) {
+
+            if(stack.getTag().contains("currentAmount") || stack.getTag().contains("stackSize")) {
                 int currentAmount;
 
-                if (stack.getTagCompound().hasKey("currentAmount")) {
-                    currentAmount = stack.getTagCompound().getInteger("currentAmount") * stack.stackSize;
+                if(stack.getTag().contains("currentAmount")) {
+                    currentAmount = stack.getTag().getInt("currentAmount") * stack.getCount();
                 } else {
-                    currentAmount = stack.getTagCompound().getInteger("stackSize") * stack.stackSize;
+                    currentAmount = stack.getTag().getInt("stackSize") * stack.getCount();
                 }
 
                 int maxStackSize = stack.getMaxStackSize();
@@ -39,8 +65,51 @@ public class Mod
                 int stacks = rest / maxStackSize;
                 int items = rest - (stacks * maxStackSize);
 
-                event.toolTip.add("§e" + DKs + " DKs §6" + stacks + " Stacks §e" + items + " Items");
+                event.getToolTip().add(new StringTextComponent("§e" + DKs + " DKs §6" + stacks + " Stacks §e" + items + " Items"));
             }
+        }
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("tooltiprechner", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m->m.getMessageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+    // Event bus for receiving Registry Events)
+    @net.minecraftforge.fml.common.Mod.EventBusSubscriber(bus= net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
         }
     }
 }
